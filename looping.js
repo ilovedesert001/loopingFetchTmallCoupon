9a33a6139669f0d1da4fe14c2f927387d38d9545
黄玄 <huxpro@gmail.com>
2015-10-15 14:22:14 +0800

    looping.js

---
+++ looping.js
@@ -0,0 +1,40 @@
+// looping fetch
+var once = function(){
+    // original author: https://v2ex.com/t/228164
+    (function(window, document) {
+        var interval = 800;
+        var closeDelay = 200;
+        var index = 0;
+        var couponLinks;
+        var getCoupon = function() {
+            if (index >= couponLinks.length) {
+                console.log("领取完毕");
+                window.once();
+                return;
+            }
+            var coponLink = couponLinks[index];
+            coponLink.click(); index++;
+            console.log("领取 第" + index + " 张");
+            setTimeout(getCoupon, interval);
+            setTimeout(function() {
+                var close = document.querySelector('.mui-dialog-close');
+                if (close != null) close.click();
+            }, closeDelay);
+        }
+        var _scrollTop = 0;
+        var _scrollStep = document.documentElement.clientHeight;
+        var _maxScrollTop = document.body.clientHeight - document.documentElement.clientHeight;
+        var autoScrollDown = setInterval(function() {
+            _scrollTop += _scrollStep;
+            if (_scrollTop > _maxScrollTop) {
+                clearInterval(autoScrollDown);
+                couponLinks = document.querySelectorAll('.mui-act-item-yhqbtn');
+                console.log("总共：" + couponLinks.length + "条张优惠券待领取...");
+                getCoupon();
+            } else {
+                document.body.scrollTop = _scrollTop;
+            }
+        }, 500);
+    }) (window, document);
+}
+once();
